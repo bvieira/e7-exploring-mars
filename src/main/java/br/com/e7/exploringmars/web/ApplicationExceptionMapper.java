1074f05be781c6ba4b8cbd507d92120406fe9f3a
Beatriz Vieira <beatriz.vieira0@walmart.com>
2017-01-07 02:53:05 -0200

    log exception name and message on application exception mapper for internal server error code

--- src/main/java/br/com/e7/exploringmars/web/ApplicationExceptionMapper.java
+++ src/main/java/br/com/e7/exploringmars/web/ApplicationExceptionMapper.java
@@ -6,11 +6,19 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.e7.exploringmars.exception.ApplicationException;
 import br.com.e7.exploringmars.model.Error;
 
 @Provider
-public class ApplicationExceptionMapper implements ExceptionMapper<ApplicationException>{
+public class ApplicationExceptionMapper implements ExceptionMapper<ApplicationException> {
+	private final Logger logger;
+	
+	public ApplicationExceptionMapper() {
+		logger = LoggerFactory.getLogger(getClass());
+	}
 
 	@Override
 	public Response toResponse(ApplicationException exception) {
@@ -19,6 +27,7 @@ public class ApplicationExceptionMapper implements ExceptionMapper<ApplicationEx
 			return response(Response.Status.BAD_REQUEST, exception.getMessage());
 		case REPOSITORY_INDEX:
 		case REPOSITORY_SEARCH:
+			logger.error("application error, exception:[{}] message:[{}]", exception.getClass().getName(), exception.getMessage());
 			return response(Response.Status.INTERNAL_SERVER_ERROR, exception.getMessage());
 		default:
 			return response(Response.Status.INTERNAL_SERVER_ERROR, exception.getMessage());
