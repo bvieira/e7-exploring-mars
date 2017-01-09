f577a61af3e09e5b46fe4bd68ed216535fd32e29
Beatriz Vieira <beatriz.vieira0@walmart.com>
2017-01-09 01:03:34 -0200

    map all exceptions, log uncaught exception

--- src/main/java/br/com/e7/exploringmars/web/WebApplicationExceptionMapper.java
+++ src/main/java/br/com/e7/exploringmars/web/WebApplicationExceptionMapper.java
@@ -6,15 +6,21 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.e7.exploringmars.model.Error;
 
 @Provider
-public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
+public class WebApplicationExceptionMapper implements ExceptionMapper<Exception> {
+	private final Logger logger = LoggerFactory.getLogger("exception-handler");
 
 	@Override
-	public Response toResponse(WebApplicationException exception) {
-		return Response.status(exception.getResponse().getStatus()).type(MediaType.APPLICATION_JSON).entity(new Error(exception.getMessage())).build();
+	public Response toResponse(Exception exception) {
+		if(exception instanceof WebApplicationException)
+			return Response.status(((WebApplicationException) exception).getResponse().getStatus()).type(MediaType.APPLICATION_JSON).entity(new Error(exception.getMessage())).build();
+		logger.error("uncaught exception", exception);
+		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON).entity(new Error(exception.getClass().getSimpleName() + ": " + exception.getMessage())).build();
 	}
 	
-
 }
